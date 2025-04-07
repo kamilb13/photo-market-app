b44da9d584052317d1b50c55becb3d728847e8a5
kamilb13 <kamil.brzoza13@gmail.com>
2025-04-07 19:51:35 +0200

    init commit

---
+++ src/main/java/pl/photomarketapp/photomarketapp/PhotoMarketAppApplication.java
@@ -0,0 +1,13 @@
+package pl.photomarketapp.photomarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoMarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoMarketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/photomarketapp/photomarketapp/PhotoMarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.photomarketapp.photomarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoMarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
