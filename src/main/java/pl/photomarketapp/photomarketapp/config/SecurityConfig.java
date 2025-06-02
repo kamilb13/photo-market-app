1da7f973ef774be78cf6ed4685b16cbe17a0740b
kamilb13 <kamil.brzoza13@gmail.com>
2025-06-02 19:10:24 +0200

    added stripe config and endpoint

--- src/main/java/pl/photomarketapp/photomarketapp/config/SecurityConfig.java
+++ src/main/java/pl/photomarketapp/photomarketapp/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 )
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login", "/auth/register", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/auth/login", "/auth/register", "/auth/google", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
--- src/main/java/pl/photomarketapp/photomarketapp/controller/PaymentController.java
+++ src/main/java/pl/photomarketapp/photomarketapp/controller/PaymentController.java
@@ -1,8 +1,54 @@
 package pl.photomarketapp.photomarketapp.controller;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.web.bind.annotation.RestController;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class PaymentController {
+    @PostMapping("/create-checkout-session")
+    public Map<String, Object> createCheckoutSession(@RequestBody ProductRequest request) throws StripeException {
+        SessionCreateParams params = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost:4200/success?idPhoto=" + request.getProductId())
+                .setCancelUrl("http://localhost:4200/cancel")
+                .addLineItem(
+                        SessionCreateParams.LineItem.builder()
+                                .setQuantity(1L)
+                                .setPriceData(
+                                        SessionCreateParams.LineItem.PriceData.builder()
+                                                .setCurrency("pln")
+                                                .setUnitAmount(request.getProductPrice())
+                                                .setProductData(
+                                                        SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                .setName(request.getProductName())
+                                                                .build()
+                                                )
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
 
+        Session session = Session.create(params);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", session.getId());
+        return response;
+    }
+
+    @Getter
+    @Setter
+    public static class ProductRequest {
+        private Long productId;
+        private String productName;
+        private Long productPrice;
+    }
 }
